cfe2279217f0391c7749014050171b8fe58f9b0e
LeonardoCN40 <leo.cabello.84@gmail.com>
2026-06-12 10:02:33 -0400

    prueba acction

--- tests/test_sandbox.py
+++ tests/test_sandbox.py
@@ -9,7 +9,7 @@ Ejecutar con: pytest -m sandbox
 
 import pytest
 
-# Prueba CI con git actions, se elimina description del flujo -
+# Prueba CI con git actions, se elimina description del codigo -
 
 @pytest.mark.sandbox
 def test_boton_id_dinamico_muestra_texto_al_hacer_click(sandbox_page):
